a6a5f30daf74df720dc0f596cbe9cae29b10e5d2
Gauri Mane <gaurimane82@gmail.com>
2026-04-09 12:09:08 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,13 +1,12 @@
-import { db } from './firebase.js';
+import { db } from "./firebase.js";
 
 import {
-collection,
-getDocs
+  collection,
+  getDocs,
 } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-
 let videos = [];
-let currentVideoId = null; 
+let currentVideoId = null;
 
 function requireAuth() {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -19,82 +18,66 @@ function requireAuth() {
 }
 
 /* Load All Videos */
-async function loadVideos(){
-if (!requireAuth()) return;
+async function loadVideos() {
+  if (!requireAuth()) return;
 
-const querySnapshot = await getDocs(
-collection(db,"videos")
-);
+  const querySnapshot = await getDocs(collection(db, "videos"));
 
-videos.length = 0;
+  videos.length = 0;
 
-querySnapshot.forEach((doc)=>{
+  querySnapshot.forEach((doc) => {
+    videos.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
 
-videos.push({
-id: doc.id,
-...doc.data()
-});
+  // Get clicked video ID
+  const params = new URLSearchParams(window.location.search);
+  const videoId = params.get("id");
 
-});
+  // Load selected video
+  if (videoId) {
+    const selectedVideo = videos.find((v) => v.id === videoId);
 
-
-// Get clicked video ID
-const params = new URLSearchParams(window.location.search);
-const videoId = params.get("id");
-
-
-// Load selected video
-if(videoId){
-
-const selectedVideo = videos.find(v => v.id === videoId);
-
-if(selectedVideo){
-loadMainVideo(selectedVideo);
+    if (selectedVideo) {
+      loadMainVideo(selectedVideo);
+    }
+  } else if (videos.length > 0) {
+    loadMainVideo(videos[0]);
+  }
 }
-
-}else if(videos.length > 0){
-
-loadMainVideo(videos[0]);
-
-}
-
-}
-
-
-
 
 /* Load Main Player */
 
-function loadMainVideo(video){
+function loadMainVideo(video) {
+  currentVideoId = video.id;
 
-currentVideoId = video.id; 
+  document.getElementById("mainVideo").src = video.url;
+  document.getElementById("title").innerText = video.title;
+  document.getElementById("subject").innerText = video.subject;
+  document.getElementById("desc").innerText = video.desc;
 
-document.getElementById("mainVideo").src = video.url;
-document.getElementById("title").innerText = video.title;
-document.getElementById("subject").innerText = video.subject;
-document.getElementById("desc").innerText = video.desc;
-
-renderSidebar();
-
+  renderSidebar();
 }
 
-
+window.redirectHome = function () {
+  window.location.href = "index.html";
+};
 /* Sidebar Render */
 
-function renderSidebar(){
+function renderSidebar() {
+  const list = document.getElementById("videoList");
 
-const list = document.getElementById("videoList");
+  list.innerHTML = "";
 
-list.innerHTML = "";
+  videos.forEach((video) => {
+    if (video.id === currentVideoId) return;
 
-videos.forEach(video=>{
+    const div = document.createElement("div");
+    div.className = "video-card";
 
-if(video.id === currentVideoId) return;
-
-const div = document.createElement("div");
-div.className = "video-card";
-
-div.innerHTML = `
+    div.innerHTML = `
 <video src="${video.url}"></video>
 <div class="video-info">
 <h4>${video.title}</h4>
@@ -102,14 +85,11 @@ div.innerHTML = `
 </div>
 `;
 
-div.onclick = ()=> loadMainVideo(video);
+    div.onclick = () => loadMainVideo(video);
 
-list.appendChild(div);
-
-});
-
+    list.appendChild(div);
+  });
 }
-
 
 /* Page Load */
 
